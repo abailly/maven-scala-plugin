c296b72fe672ccaa18e53deeffca37c6f2a74886
Josh Suereth <Joshua.Suereth@gmail.com>
2008-11-12 23:39:37 -0500

    Added support for compiler plugin dependencies

--- src/main/java/org/scala_tools/maven/ScalaMojoSupport.java
+++ src/main/java/org/scala_tools/maven/ScalaMojoSupport.java
@@ -100,6 +100,29 @@ abstract class ScalaMojoSupport extends AbstractMojo {
     protected BasicArtifact[] dependencies;
 
     /**
+     * Compiler plugin dependencies to use when compiling.
+     * ex:
+     * @parameter
+     * <xmp>
+     *    <compilerPlugins>
+     *       <dependency>
+     *          <groupId>my.scala.plugin</groupId>
+     *          <artifactId>amazingPlugin</artifactId>
+     *          <version>1.0-SNAPSHOT</version>
+     *       </dependency>
+     *    </compilerPlugins>
+     * </xmp>
+     */    
+    protected BasicArtifact[] compilerPlugins;
+    //TODO - Use this instead of above, if possible...
+//    /**
+//     * @parameter expression="${plugin.artifacts}"
+//     * @readonly
+//     * @since 1.1-beta-1
+//     */
+//    private List pluginDependencies;
+    
+    /**
      * Jvm Arguments.
      *
      * @parameter
@@ -252,6 +275,7 @@ abstract class ScalaMojoSupport extends AbstractMojo {
     protected JavaCommand getScalaCommand() throws Exception {
         JavaCommand cmd = getEmptyScalaCommand(scalaClassName);
         cmd.addArgs(args);
+        addCompilerPluginOptions(cmd);
         cmd.addJvmArgs(jvmArgs);
         return cmd;
     }
@@ -273,6 +297,31 @@ abstract class ScalaMojoSupport extends AbstractMojo {
             }
         }
         return JavaCommand.toMultiPath(classpath.toArray(new String[classpath.size()]));
+    }
+    
+    private void addCompilerPluginOptions(JavaCommand scalac) throws Exception {
+    	for(String plugin : getCompilerPlugins()) {
+    		scalac.addArgs("-Xplugin:"+plugin);
+    	}
+    }
+    
+    private Set<String> getCompilerPlugins() throws Exception {
+    	Set<String> plugins = new HashSet<String>();
+    	if(compilerPlugins != null) {
+    		Set<String> ignoreClasspath = new HashSet<String>();
+    		 addToClasspath(SCALA_GROUPID, "scala-compiler", scalaVersion, ignoreClasspath);
+    		 addToClasspath(SCALA_GROUPID, SCALA_LIBRARY_ARTIFACTID, scalaVersion, ignoreClasspath);   		
+    		
+    		for(BasicArtifact artifact : compilerPlugins) {
+    			Set<String> pluginClassPath = new HashSet<String>();
+    			addToClasspath(artifact.groupId, artifact.artifactId, artifact.version, pluginClassPath);
+    			//TODO - Ensure only one item on classpath, or that we pull the *CORRECT* item.
+    			pluginClassPath.removeAll(ignoreClasspath);
+    			plugins.add(pluginClassPath.iterator().next());
+    		}
+    	}
+    	
+    	return plugins;
     }
 
     private String getBootClasspath() throws Exception {
